9fbfad3ea642bfa0b89794f39665f4ba45d352c5
satsuki-thyme <satsuki.thyme@gmail.com>
2023-02-03 19:54:42 +0900

    正規表現を修正

--- novelparse.js
+++ novelparse.js
@@ -17,11 +17,10 @@ function novelparse(src, newLineMode) {
   .replace(/([々〇〻\u3400-\u9FFF\uF900-\uFAFF\uD840-\uD87F\uDC00-\uDFFF]+)《(.+?)》/g, '<ruby>$1<rt>$2</rt></ruby>')
   .replace(/([々〇〻\u3400-\u9FFF\uF900-\uFAFF\uD840-\uD87F\uDC00-\uDFFF]+)\(([\u3040-\u309F\u30A0-\u30FF]+?)\)/g, '<ruby>$1<rt>$2</rt></ruby>')
   .replace(/([々〇〻\u3400-\u9FFF\uF900-\uFAFF\uD840-\uD87F\uDC00-\uDFFF]+)（([\u3040-\u309F\u30A0-\u30FF]+?)）/g, '<ruby>$1<rt>$2</rt></ruby>')
-  .replace(/[|｜](.*?)[|｜]《(.*?)》/g, `$1《$2》`)
-  .replace(/[|｜]《(.*?)》/g, '《$1》')
-  .replace(/[|｜]\((.*?)\)/g, '($1)')
-  .replace(/[|｜]（(.*?)）/g, '（$1）')
-  .replace(/#(.*?)__(.*?)__#/g, `<ruby>$1<rt>$2</rt></ruby>`)
+  .replace(/[|｜]《(.+?)》/g, '《$1》')
+  .replace(/[|｜]\((.+?)\)/g, '($1)')
+  .replace(/[|｜]（(.+?)）/g, '（$1）')
+  .replace(/#(.+?)__(.+?)__#/g, `<ruby>$1<rt>$2</rt></ruby>`)
   if (newLineMode === "few") {
     work = work
     .replace(rx0, rly => {
